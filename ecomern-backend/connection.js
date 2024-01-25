c70374e053447a21cc41a28de7d2866c3504f212
kaymaskaymas <114069552+kaymaskaymas@users.noreply.github.com>
2024-01-25 12:29:46 +0530

    Add files via upload

---
+++ ecomern-backend/connection.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const connectionStr = process.env.MONGO_API
+
+mongoose.connect(connectionStr, {useNewUrlparser: true})
+.then(() => console.log('connected to mongodb'))
+.catch(err => console.log(err))
+
+mongoose.connection.on('error', err => {
+  console.log(err)
+})
